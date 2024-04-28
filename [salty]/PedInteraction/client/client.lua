5bc003c708b93426c8eb97ca16e5f02f63de3f2e
Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2024-04-28 23:31:25 +1000

    Added radius spawn

--- [salty]/PedInteraction/client/client.lua
+++ [salty]/PedInteraction/client/client.lua
@@ -11,6 +11,19 @@ RegisterCommand('spawnpedline', function(_, args)
     TriggerServerEvent('PedInteraction:spawnpedline', pedAmount)
 end)
 
+RegisterCommand('spawnpedradius', function(_, args)
+    local pedAmount = tonumber(args[1])
+
+    if not pedAmount then
+        TriggerEvent('chat:addMessage', {
+            args = { 'Invalid ped amount. Please input a number', },
+        })
+        return
+    end 
+
+    TriggerServerEvent('PedInteraction:spawnpedradius', pedAmount)
+end)
+
 RegisterNetEvent('PedInteraction:spawnline')
 AddEventHandler('PedInteraction:spawnline', function(pedAmount)
     local playerPed = PlayerPedId()
@@ -40,17 +53,72 @@ AddEventHandler('PedInteraction:spawnline', function(pedAmount)
 
 
     TriggerEvent('chat:addMessage' , {
-        args = {tostring(pedAmount).. ' peds spawning on client',}
+        args = {tostring(pedAmount).. ' peds spawning in a line on player',}
     })
 
     for i = 1, pedAmount do
+        local spawnX = spawnPos.x
+        local spawnY = spawnPos.y + i
+        local spawnZ = spawnPos.z
         local model = pedModels[math.random(#pedModels)]
-        local ped = CreatePed(0, model, spawnPos.x, spawnPos.y + i, spawnPos.z, 0, true, false)
+        local ped = CreatePed(0, model, spawnX, spawnY, spawnZ, 0, true, false)
         SetPedRandomComponentVariation(ped, true)
         SetPedAsNoLongerNeeded(ped)
         SetModelAsNoLongerNeeded(model)
     end
 end)
+
+RegisterNetEvent('PedInteraction:spawnradius')
+AddEventHandler('PedInteraction:spawnradius', function(pedAmount)
+    local playerPed = PlayerPedId()
+    local spawnPos = GetEntityCoords(playerPed)
+    local radius = 10
+
+    local pedModels = {
+        `a_f_y_tourist_01`,
+        `a_m_m_business_01`,
+        `a_f_m_bodybuild_01`,
+        `s_m_y_cop_01`,
+        `a_m_m_fatlatin_01`,
+        `a_m_m_soucent_03`,
+        `a_m_o_soucent_03`,
+        `a_m_y_runner_01`,
+    }
+
+    function EnsureModelisLoaded(models)
+        for _, model in ipairs(models) do
+            RequestModel(model)
+            while not HasModelLoaded(model) do
+                Wait(500)
+            end
+        end
+    end
+
+    EnsureModelisLoaded(pedModels)
+
+
+    TriggerEvent('chat:addMessage' , {
+        args = {tostring(pedAmount).. ' peds spawning in a radius around player',}
+    })
+
+    for i = 1, pedAmount do
+        local angle = math.random() * 360
+        local radian = math.rad(angle)
+        local randomRadius = math.random() * radius
+        local offsetX = math.cos(radian) * randomRadius
+        local offsetY = math.sin(radian) * randomRadius
+        local spawnX = spawnPos.x + offsetX
+        local spawnY = spawnPos.y + offsetY
+        local spawnZ = spawnPos.z
+
+        local model = pedModels[math.random(#pedModels)]
+        local ped = CreatePed(0, model, spawnX, spawnY, spawnZ, 0, true, false)
+        SetPedRandomComponentVariation(ped, true)
+        SetPedAsNoLongerNeeded(ped)
+        SetModelAsNoLongerNeeded(model)
+    end
+end)
+
 
 RegisterCommand('explodeped', function(_, args)
     local pedRadius = args[1]
--- [salty]/PedInteraction/server/server.lua
+++ [salty]/PedInteraction/server/server.lua
@@ -12,6 +12,20 @@ RegisterNetEvent('PedInteraction:spawnpedline', function(pedAmount)
     TriggerClientEvent('PedInteraction:spawnline', playerId, pedNumber)
 end)
 
+RegisterNetEvent('PedInteraction:spawnpedradius', function(pedAmount)
+    local playerId = source
+    local pedNumber = tonumber(pedAmount)
+    
+    if pedNumber <= 0 then
+        TriggerClientEvent('chat:addMessage', playerId {
+            args = { 'Number of peds must be greater than 0', },
+        })
+        return
+    end
+
+    TriggerClientEvent('PedInteraction:spawnradius', playerId, pedNumber)
+end)
+
 RegisterNetEvent('PedInteraction:explodeped', function(pedRadius)
     local playerId = source
     local radius = tonumber(pedRadius)
